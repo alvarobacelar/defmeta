0989264183fddf05b6fefc3072d1e2c82067edc2
Álvaro Bacelar <alvaro@infoway-pi.com.br>
2017-03-01 16:10:42 -0300

    Adicionado filtro na pagina home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,20 @@
 class WelcomeController < ApplicationController
+
+  has_scope :periodo
+
   def index
-    @metas = Meta.periodo(Date.today).count
+    if params[:periodo]
+      @mes_referencia = params[:periodo].to_date
+    else
+      @mes_referencia = Date.today
+    end
+    # FIXME Acrescentado para evitar bugs nos dias > 28
+    #       Deve ser corrigido pois pode afetar o fluxo de caixa do mês
+    @mes_referencia = @mes_referencia.change(day: 1)
+    data_corrente = Date.today
+    params[:periodo] = l(data_corrente) unless params[:periodo]
+    # @metas = Meta.periodo(Date.today).count
+    @metas = apply_scopes(Meta).all.order('pontuacao DESC').count
     @todas_metas = Meta.periodo(Date.today)
   end
 end
