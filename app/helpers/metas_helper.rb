1a3b1264716b17a0f2cb7bbf68c82e8552b2e253
Álvaro Bacelar <alvaro@infoway-pi.com.br>
2017-12-26 10:46:34 -0300

    Ajustado o status da meta

--- app/helpers/metas_helper.rb
+++ app/helpers/metas_helper.rb
@@ -1,11 +1,11 @@
 module MetasHelper
-
+  
   def andamento(id)
     conta_detalhamento = Detalhamento.where(meta_id: id).count
     conta_concluida = Detalhamento.where(meta_id: id, status: true).count
     porcento_concluida = conta_concluida.to_f / conta_detalhamento.to_f * 100
   end
-
+  
   def background_meta(meta)
     if meta == 3
       "background-color:  #6aa84f; color: #000;"
@@ -15,17 +15,20 @@ module MetasHelper
       "background-color:  #d9ead3; color: #000;"
     end
   end
-
+  
   def concluido(porcento)
-    porcento = 0
-    if porcento == 100
-      "success"
-    elsif porcento < 100 && porcento > 0
-      "warning"
+    if porcento.present?
+      if porcento == 100
+        "success"
+      elsif porcento < 100 && porcento > 0
+        "warning"
+      elsif porcento == 0
+        "danger"
+      end
     else
       "danger"
     end
   end
   
-
+  
 end
